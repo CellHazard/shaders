7a6cda6624f53d05dc0a71e2abf841abc82997d6
splinesen <splinesen@gmail.com>
2024-05-22 18:58:13 +0300

    Optimizations (3x faster)
    numpy parallelization and memory optimizations resulting in a 3x performance boost.

--- sine_shader.py
+++ sine_shader.py
@@ -6,14 +6,18 @@ import math
 import time
 from numba import jit
 
+
 '''
 Copyright by splinesen.
 GitHub: splinesen
+
+Runs at 85~ FPS at 1080p on my high end PC.
 '''
 
+
 # Set canvas dimensions (same as in ShaderToy).
-canvas_width = 1120
-canvas_height = 630
+canvas_width = 1920
+canvas_height = 1080
 
 # Initialize pygame
 pygame.init()
@@ -29,12 +33,13 @@ combinedIntensity = 150.0
 waveZoom = 4.0
 waveStretch = 1.5
 
-
-@jit(nopython=True)
+@jit(nopython=True, parallel=True)
 def generate_uv_shader(canvas_width, canvas_height, iTime):
     pixels = np.zeros((canvas_width, canvas_height, 3), dtype=np.uint8)
 
-    for y in range(canvas_height):
+    sin_iTime = math.sin(iTime * 0.33)
+    cos_iTime = math.cos(iTime * 0.33)
+    for y in numba.prange(canvas_height):
         # Map y-coordinate to OpenGL's coordinate system
         ogl_y = (canvas_height - y) - 1
 
@@ -47,20 +52,15 @@ def generate_uv_shader(canvas_width, canvas_height, iTime):
             u0 = waveZoom * u
             v0 = waveZoom * v
 
-            # finalCol
-            r = 0
-            g = 0
-            b = 0
-
             v0 += waveStretch * math.sin(u0 - (iTime * 0.75))
 
             lineIntensity = minIntensity + (maxIntensity * abs((u + iTime) % 2.0 - 1.0))
 
             glowWidth = abs(lineIntensity / (combinedIntensity * v0))
 
-            r += glowWidth * (1.0 + math.sin(iTime * 0.33))
-            g += glowWidth * (1.0 - math.sin(iTime * 0.33))
-            b += glowWidth * (1.0 - math.cos(iTime * 0.33))
+            r = glowWidth * (1.0 + sin_iTime)
+            g = glowWidth * (1.0 - sin_iTime)
+            b = glowWidth * (1.0 - cos_iTime)
 
             # Convert to RGB values in the range [0, 255]
             r = int(min(max(r * 255, 0), 255))
@@ -84,13 +84,12 @@ while running:
             running = False
 
     elapsed_time = time.time() - start_time
-    formatted_time = f"{elapsed_time:.3f}"
-    iTime = float(formatted_time)
+    iTime = float(f"{elapsed_time:.3f}")
 
     # Generate the UV shader pixel array.
     random_pixels = generate_uv_shader(canvas_width, canvas_height, iTime)
 
-     # Create a pygame Surface from the numpy array.
+    # Create a pygame Surface from the numpy array.
     surface = pygame.surfarray.make_surface(random_pixels)
 
     # Blit the surface onto the canvas.
